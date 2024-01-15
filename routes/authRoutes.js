cd6e954526007f1c5fbf6480c054c92bbc70a351
Pranav Kumar <pranavkumar97954@gmail.com>
2024-01-15 23:06:55 +0530

    Second commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
 const { register, login } = require("../controllers/authControllers");
-const { checkUser,checkAdminUser } = require("../middlewares/authMiddleware");
+const { checkUser } = require("../middlewares/authMiddleware");
 
 const router = require("express").Router();
 
 router.post("/query", checkUser); 
 router.post("/register", register);
 router.post("/login", login);
-//router.post("/admin", checkUser); 
-router.post("/admin", checkAdminUser); 
+router.post("/admin", checkUser); 
 module.exports = router;
