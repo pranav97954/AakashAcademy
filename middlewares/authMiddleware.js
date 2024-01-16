a8e97f9624c0488d965cdd9256a997a8e46c35ef
Pranav Kumar <pranavkumar97954@gmail.com>
2024-01-16 09:28:54 +0530

    auth

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,34 @@
 const User = require("../model/authModel");
 const jwt = require("jsonwebtoken");
 
-module.exports.checkUser = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (token) {
-    jwt.verify(
-      token,
-      "kishan sheth super secret key",
-      async (err, decodedToken) => {
-        if (err) {
-          res.json({ status: false });
-          next();
-        } else {
-          const user = await User.findById(decodedToken.id);
-          if (user) res.json({ status: true, user: user.email });
-          else res.json({ status: false });
-          next();
-        }
-      }
-    );
-  } else {
-    res.json({ status: false });
-    next();
+module.exports.checkUser = async (req, res) => {
+  try {
+    const token = req.cookies.jwt;
+    
+    if (!token) {
+      return res.json({ status: false });
+    }
+
+    const decodedToken = jwt.verify(token, "kishan sheth super secret key");
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+      return res.json({ status: false });
+    }
+
+    // User is authenticated
+    return res.json({
+      status: true,
+      user: {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        // Include any other user properties needed
+      },
+    });
+  } catch (error) {
+    console.error("JWT verification error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
   }
 };
